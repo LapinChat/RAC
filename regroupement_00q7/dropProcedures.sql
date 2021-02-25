30d15957b7e81306bd998ef2915a2ae38eee9ce3
Pierre Tremblay-Thériault <pierre@lapinchatware.ca>
2021-02-24 21:37:34 -0500

    Procedures: Added count procedures for all tables. Gave some users permissions to use them. Added drop statement for those procedures.

--- regroupement_00q7/dropProcedures.sql
+++ regroupement_00q7/dropProcedures.sql
@@ -1,3 +1,20 @@
 USE regroupement00q7;
 
 DROP PROCEDURE IF EXISTS get_role_permissions;
+DROP PROCEDURE IF EXISTS count_address;
+DROP PROCEDURE IF EXISTS count_addresstype;
+DROP PROCEDURE IF EXISTS count_country;
+DROP PROCEDURE IF EXISTS count_location;
+DROP PROCEDURE IF EXISTS count_logflight;
+DROP PROCEDURE IF EXISTS count_logpart;
+DROP PROCEDURE IF EXISTS count_logpartaction;
+DROP PROCEDURE IF EXISTS count_part;
+DROP PROCEDURE IF EXISTS count_parttype;
+DROP PROCEDURE IF EXISTS count_permission;
+DROP PROCEDURE IF EXISTS count_quadcopter;
+DROP PROCEDURE IF EXISTS count_role;
+DROP PROCEDURE IF EXISTS count_roles_permissions;
+DROP PROCEDURE IF EXISTS count_state;
+DROP PROCEDURE IF EXISTS count_user;
+DROP PROCEDURE IF EXISTS count_user_addresses;
+DROP PROCEDURE IF EXISTS count_user_roles;
--- regroupement_00q7/procedures.sql
+++ regroupement_00q7/procedures.sql
@@ -1,5 +1,6 @@
 USE regroupement00q7;
 
+-- get_role_permissions(string role_name)
 DELIMITER //
 
 CREATE OR REPLACE PROCEDURE get_role_permissions(
@@ -12,6 +13,98 @@ CREATE OR REPLACE PROCEDURE get_role_permissions(
         INNER JOIN permission AS p ON rp.permission_id = p.id
     WHERE
         r.name = role_name;
+END //
+
+-- COUNT TABLES ENTRIES
+CREATE OR REPLACE PROCEDURE count_address()
+BEGIN
+    SELECT COUNT(*) FROM address;
+END //
+
+CREATE OR REPLACE PROCEDURE count_addresstype()
+BEGIN
+    SELECT COUNT(*) FROM addresstype;
+END //
+
+CREATE OR REPLACE PROCEDURE count_country()
+BEGIN
+    SELECT COUNT(*) FROM country;
+END //
+
+CREATE OR REPLACE PROCEDURE count_location()
+BEGIN
+    SELECT COUNT(*) FROM location;
+END //
+
+CREATE OR REPLACE PROCEDURE count_logflight()
+BEGIN
+    SELECT COUNT(*) FROM logflight;
+END //
+
+CREATE OR REPLACE PROCEDURE count_logpart()
+BEGIN
+    SELECT COUNT(*) FROM logpart;
+END //
+
+CREATE OR REPLACE PROCEDURE count_logpartaction()
+BEGIN
+    SELECT COUNT(*) FROM logpartaction;
+END //
+
+CREATE OR REPLACE PROCEDURE count_part()
+BEGIN
+    SELECT COUNT(*) FROM part;
+END //
+
+CREATE OR REPLACE PROCEDURE count_parttype()
+BEGIN
+    SELECT COUNT(*) FROM parttype;
+END //
+
+CREATE OR REPLACE PROCEDURE count_permission()
+BEGIN
+    SELECT COUNT(*) FROM permission;
+END //
+
+CREATE OR REPLACE PROCEDURE count_quadcopter()
+BEGIN
+    SELECT COUNT(*) FROM quadcopter;
+END //
+
+CREATE OR REPLACE PROCEDURE count_role()
+BEGIN
+    SELECT COUNT(*) FROM role;
+END //
+
+CREATE OR REPLACE PROCEDURE count_state()
+BEGIN
+    SELECT COUNT(*) FROM state;
+END //
+
+CREATE OR REPLACE PROCEDURE count_user()
+BEGIN
+    SELECT COUNT(*) FROM user;
+END //
+
+-- COUNT TABLES ENTRIES - JOINT TABLES
+CREATE OR REPLACE PROCEDURE count_address_addresstypes()
+BEGIN
+    SELECT COUNT(*) FROM address_addresstypes;
+END //
+
+CREATE OR REPLACE PROCEDURE count_roles_permissions()
+BEGIN
+    SELECT COUNT(id) FROM roles_permissions;
+END //
+
+CREATE OR REPLACE PROCEDURE count_user_addresses()
+BEGIN
+    SELECT COUNT(id) FROM user_addresses;
+END //
+
+CREATE OR REPLACE PROCEDURE count_user_roles()
+BEGIN
+    SELECT COUNT(id) FROM user_roles;
 END //
 
 DELIMITER ;
